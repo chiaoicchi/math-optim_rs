75872c79c56d3da50a46cd668fe47fc0c3cbf512
chiaoi <chiaoi@pm.me>
2025-10-27 02:34:05 +0900

    fix:fix doc

--- src/algebra/monoid.rs
+++ src/algebra/monoid.rs
@@ -11,7 +11,7 @@
 //!
 //! - **associativity**: for all `a, b, c` in `S`, the equation `(a * b) * c == a * (b * c)` holds.
 //! - **identity element**: There exists an element `e` in S such that for every element `a` in `S`,
-//! the equalities `a * e == e * a == a` hold.
+//!   the equalities `a * e == e * a == a` hold.
 
 /// A trait representating a *monoid*.
 ///
